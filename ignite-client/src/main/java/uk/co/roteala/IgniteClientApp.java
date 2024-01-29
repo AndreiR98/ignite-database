a4db931a581781deab91428ea3529d315150bddf
Roti <rotaru.andrei98@gmail.com>
2024-01-29 12:34:02 +0000

    chore: Fix kubernetes deployments

---
+++ ignite-client/src/main/java/uk/co/roteala/IgniteClientApp.java
@@ -0,0 +1,12 @@
+package uk.co.roteala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteClientApp
+{
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteClientApp.class, args);
+    }
+}
